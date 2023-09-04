a9895ff2e123b2f6fa88b2498a4c5ea620bcb59b
Mirna Rodriguez <60165995+MirnaLety@users.noreply.github.com>
2023-09-03 22:59:27 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinch and Rocket Smothie')
-streamlit.text('🐔 🥑🍞Hard Boiled Free Range Egg')
-
+streamlit.text('🐔 🥑🍞 Hard Boiled Free Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
