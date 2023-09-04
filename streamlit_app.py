0c088ddbdc8b47ad8f90f43c022322831dac1e53
Mirna Rodriguez <60165995+MirnaLety@users.noreply.github.com>
2023-09-03 22:47:25 -0600

    Update streamlit_app.py
    Fixed Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & and Blueberry Oatmeal')
+streamlit.text('Kale, Spinch and Rocket Smothie')
+streamlit.text('Hard Boiled Free Range Egg')
+
