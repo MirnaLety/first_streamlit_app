4e8b464153225a9866d3233533f312739a04f1cc
Mirna Rodriguez <60165995+MirnaLety@users.noreply.github.com>
2023-09-17 17:06:39 -0600

    12.1 Chall Text Box

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,7 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+#Challenge 12.1
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('Thanks for adding jackfruit ', add_my_fruit)
