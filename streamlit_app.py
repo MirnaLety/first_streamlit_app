c18b19c01483bb9c5db64e9a1c2b08dd41401980
Mirna Rodriguez <60165995+MirnaLety@users.noreply.github.com>
2023-09-17 17:36:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 #Libraries:
-#import streamlit
-#import pandas
-#import requests
-#import snowflake.connector
+import streamlit
+import pandas
+import requests
+import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
