9373571d78f222014fd9c167941d1df237abe242
Mirna Rodriguez <60165995+MirnaLety@users.noreply.github.com>
2023-09-17 20:05:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,9 +55,6 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
-
 #Challenge 12.1
 
 #Allow the end user to add a fruit to the list
@@ -73,5 +70,6 @@ if streamlit.button('Add a Fruit to the List'):
   streamlit.write(back_from_funtion)
 
 #This will not work correctly, but just go with it for now
-
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 
